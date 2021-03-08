c7782412282b55f2140cbca07780330329e45f38
odin568 <25813640+odin568@users.noreply.github.com>
2021-03-08 11:49:35 +0100

    Update install.bat

--- deploy/winsw/install.bat
+++ deploy/winsw/install.bat
@@ -1,12 +1,25 @@
+@echo off
+
+@echo Checking for Java...
+where java >nul 2>nul
+if %errorlevel%==1 (
+    @echo Java not found in path.
+    pause
+    exit
+)
+
+@echo Creating output directory if it does not exist...
 if not exist "maps" mkdir maps
 
+@echo Installing Service...
 FE2_Kartengenerierung.exe install
 FE2_Kartengenerierung.exe status
+
+@echo Starting Service...
 FE2_Kartengenerierung.exe start
 FE2_Kartengenerierung.exe status
 
+@echo Give Service some seconds to come up. Will then open health endpoint...
 timeout /t 10 /nobreak
 
 start "" http://localhost:8080/actuator/health
-
-pause
